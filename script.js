3f02b86fbfcd7f6f16376fec0872e4a532253ef3
adriana-chirila <chirila.mihaela.adriana@gmail.com>
2017-11-28 10:17:35 +0200

    Add first version of app

---
+++ script.js
@@ -0,0 +1,48 @@
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    if(window.innerWidth < 630){
+        document.getElementById("search").style.width = "0px";
+    }
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("search").style.width = "250px";
+}
+
+function toggleNav(x) {
+    if (x.classList.toggle("change")) {
+        openNav();
+    }
+    else {
+        closeNav();
+    }
+}
+
+function initialize() {
+    var mapProp = {
+        center: new google.maps.LatLng(51.508742, -0.120850),
+        zoom: 5,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
+
+
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
